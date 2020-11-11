76daaefacf68d35ad1f28cb2883cfd61ef5055d1
mattmck <matthew.mckerroll@gmail.com>
2020-11-10 23:01:44 -0500

    reformatted readme file, changed some localstorage stuff, added in some css, added in script.js comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
 moment().format("L");
 
 function search(city){
-    city = city.trim();
-    //console.log(city)
+    city = city.trim(); //got to get rid of those pesky spaces
+
     askURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=8c597b19af9603b4a730899ac64b0a8c";
     fiveday = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&cnt=5&units=metric&appid=8c597b19af9603b4a730899ac64b0a8c";
 
@@ -13,8 +13,7 @@ $.ajax({
         url: askURL,
         method: 'GET'
     }).then(function (response) {
-       // console.log(response);
-        //console.log(askURL);
+
         
         //empties divs to clear out previously entered stuff
         $("#currentWeather").empty();
@@ -27,11 +26,11 @@ $.ajax({
         //var weatherNow = response.weather[0].icon;
         
 
-        //temp
+        //storing temp
         var tempNow =$("<p>").text("Temperature: " + response.main.temp+"°C");
-        //humidity
+        //storing humidity
         var humidity = $("<p>").text("Humidity: " +  response.main.humidity + "%");
-        //wind
+        //storing wind
         var wind = $("<p>").text("Wind Speed: " + response.wind.speed + "Kmph");
 
         //uses the icon property of the weather object to select the link to the icon
@@ -47,13 +46,13 @@ $.ajax({
         $("#currentWeather").html(mainWthr);
 
 
-        //UV forcast api call
+        
 
         var lat = response.coord.lat;
         var lon = response.coord.lat;
 
         var uvURL = "https://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid=8c597b19af9603b4a730899ac64b0a8c";
-
+    //UV forcast api call
         $.ajax({
             url: uvURL,
             method: 'GET'
@@ -64,53 +63,61 @@ $.ajax({
 
             //this if-else tree decides the color of the UV index number to indicate severity
             if (uvIndex<3) {
-                //green
+                //green = favorable
                 var uvEl = $("<p>").text("UV Index: ");
                 var uvNum = $("<button class='btn bg-success'>").text(response.value);
                 uvEl.append(uvNum);
             } else if(uvIndex=>3&&uvIndex<6) {
-                //yellow
+                //yellow = moderate
                 var uvEl = $("<p>").text("UV Index: ");
                 var uvNum = $("<button class='btn bg-warning'>").text(response.value);
                 uvEl.append(uvNum);
             } else{
-                //red
+                //red = severe
                 var uvEl = $("<p>").text("UV Index: ");
                 var uvNum = $("<button class='btn bg-danger'>").text(response.value);
                 uvEl.append(uvNum);
             }
             
             $("#uvLight").html(uvEl);
-            console.log("this is the uvel " + uvEl);
+            
 
         
         });
-    
+        
+        
+        //API call for the 5 day forcast starts here
         $.ajax({
             url: fiveday,
             method: 'GET'
         }).then(function (response) {
             //grab the daily weather data, and put it into an array
             var dayArr = response.list;
-            $("#daily5").empty();
+            $("#daily5").empty(); //clear the contents incase anything was here
 
+            //loop through each day constructing each div one at a time
             for (var i = 0; i < dayArr.length; i++) {
                 var dailydiv = $("<div class='card shadow-lg text-white bg-primary mx-auto mb-10 p-2' style='width: 8.5rem; height: 11rem;'>");
                 
+                //here i get the date info and use only the YYYY-MM-DD part, trimming the time
                 var date5 = dayArr[i].dt_txt;
                 var dateOnly = date5.substr(0,10)
                 var temp = dayArr[i].main.temp;
                 var humidex = dayArr[i].main.humidity;
 
+                //here i stick on the date, temp, and humidity
                 var h5date = $("<h5 class='card-title'>").text(dateOnly);
-                var dailyTemp = $("<p class='card-text'>").text("Temp: " + temp);
-                var dailyHum = $("<p class='card-text'>").text("Humidity: " + humidex);
+                var dailyTemp = $("<p class='card-text'>").text("Temp: " + temp+"°C");
+                var dailyHum = $("<p class='card-text'>").text("Humidity: " + humidex+"%");
 
+                //here the weather icon is identified, since the API returns the code for it
                 var wthIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/"+dayArr[i].weather[0].icon+".png")
 
+                //giving the icon a size.
                 wthIcon.attr("style", "height: 40px; width: 40px");
 
-                dailydiv.append(dateOnly);
+                //this builds the daily forcast divs
+                dailydiv.append(h5date);
                 dailydiv.append(wthIcon);
                 dailydiv.append(dailyTemp);
                 dailydiv.append(dailyHum);
@@ -130,33 +137,38 @@ $.ajax({
     
 }
 
-function init(){
-var prevSearch = JSON.parse(localStorage.getItem("city"));
-console.log(prevSearch)
-var searchDiv = $("<button class='border border-dark rounded btn'>").text(prevSearch);
-var searchlistDiv = $("<div>");
-searchlistDiv.append(searchDiv);
 
-$("#searchlist").prepend(searchlistDiv);
+//runs when the page loads
+function init(){
+
+    //get last entered search item and make the correstponding elements for it.
+    var prevSearch = JSON.parse(localStorage.getItem("city"));
+    
+    var searchDiv = $("<button class='border border-dark rounded btn'>").text(prevSearch);
+    var searchlistDiv = $("<div>");
+    searchlistDiv.append(searchDiv);
+
+    $("#searchlist").prepend(searchlistDiv);
 }
 
 
-init();
+init(); 
 
 
 //run when the user hits the search button.
 $("#citySelection").on("click", function(event){
-    event.preventDefault();
+    event.preventDefault(); //prevent default action 
 
-    var cityList = [];
+    var cityList = []; //list that will contain the previously searched city.
 
     var cityText = $("#cityInput").val().trim();
 
     var listText = $(this).siblings("input").val();
+   // console.log($(this).siblings("input").val());
 
     cityList.push(listText);
 
-    console.log(listText)
+    //storing the info in the browser.
     localStorage.setItem("city", JSON.stringify(cityList));
 
     search(cityText);
--- README.md
+++ README.md
@@ -1,92 +1,60 @@
-# weatherDashboard
-Dashboard that gives you weather data.
-# 06 Server-Side APIs: Weather Dashboard
+# Weather Dashboard
+by Matthew Mckerroll
 
-## Your Task
+## Table of Contents
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+* [Description](#Description)
+* [Screenshot](#Screenshot)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that provides basic setup and usage instructions. You will use `localStorage` to store any persistent data.
+## Description
 
-## User Story
+This site is for Assignment 6 for the coding bootcamp, this is a site where users can search for a city, and after hitting the search button, then they will be able to the current weather, as well as the 5 day forcast.
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+## Screenshot
+![Image of site](./Assets/Screenshot_of_site.png)
 
-## Acceptance Criteria
+## Installation
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+visit the site [here](https://mattmckerroll.github.io/weatherDashboard/)
 
-## Mock-Up
 
-The following image shows the web application's appearance and functionality:
+## Usage 
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+Type in whatever city you choose, and then hit search. After searching for some cities
 
-## Grading Requirements
 
-This homework is graded based on the following criteria: 
+## Credits
 
-### Technical Acceptance Criteria: 40%
+* Matthew McKerroll
+* W3 Schools are a big help: [W3Schools](https://www.w3schools.com/)  
+* shoutout to [stackoverflow](https://stackoverflow.com/)
+* MDN web docs is very helpful for some JQuery stuff [MDN](https://developer.mozilla.org/en-US/)
 
-* Satisfies all of the above acceptance criteria plus the following:
+## License
 
-    * Uses the OpenWeather API to retrieve weather data.
+![MIT](https://img.shields.io/apm/l/atomic-design-ui.svg?)
 
-    * Uses `localStorage` to store persistent data.
+Copyright (c) 2020 Matthew Mckerroll
 
-### Deployment: 32%
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-* Application deployed at live URL.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-* Application loads with no errors.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2020 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
